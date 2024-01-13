c62bd77a51090d7af5d0c96d6a4cf793f5baf32a
Mike <mike@exnihilodigital.com>
2024-01-13 07:52:46 -0700

    cleanup

--- _js/theme.js
+++ _js/theme.js
@@ -271,10 +271,10 @@ const flexTestimonialSlider = ($) => {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        autoplay: {
-            delay: 8000,
-            disableOnInteraction: false,
-        },
+        // autoplay: {
+        //     delay: 12000,
+        //     disableOnInteraction: false,
+        // },
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
